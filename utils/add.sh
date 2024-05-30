55861d63262724078415150d687c868c97d076ad
eucarizan <eucarizan@gmail.com>
2024-05-30 15:13:39 +0800

    chore: updated add problem script

--- utils/add.sh
+++ utils/add.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
+read -d '' details << EOF
+<details>
+
+### description
+
+### solution
+
+</details>
+
+EOF
+
 if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
     echo "please provide a \$1: filename / \$2: readme details line number / \$3: problem name"
-elif [[ ! -f ../utils/details ]]; then
-    echo "missing details file"
+#elif [[ ! -f ../../../utils/details ]]; then
+#    echo "missing details file"
 else
     echo "creating js file"
     touch $1.js
     echo "updating readme"
-    cat ../utils/details | sed -i "$2r /dev/stdin" README.md
+#    cat ../../../utils/details | sed -i "$2r /dev/stdin" README.md
+    echo "$details" | sed -i "$2r /dev/stdin" README.md
     sed -i "$(($2+1))i ## $3" README.md
     sed -i "$(($2+2))i [$3](#$3)" README.md
     sed -i "$(($2+4))i <summary>$3</summary>" README.md
